4d627fbffca7409f91105835ecb89f90ad251fb9
root <miljan8558@gmail.com>
2015-12-18 09:42:24 +0100

    make it better

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,7 +1,16 @@
 class PlayersController < ApplicationController
 
   ## 
-  def song1
-    
+  def song1    
   end
+
+  def song2
+  end
+
+  def song3    
+  end
+
+  def song_all
+  end      
+  
 end
